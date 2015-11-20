83070e95d5580fb1fec0cbb62859c0db25f4c702
TaeGu <mykimtaegu@naver.com>
2015-11-20 22:21:51 +0900

    버전 0.0.7, 8, 9

---
+++ SmartLocking(V.0.0.7)/src/com/example/smartlocking/LockScreenActivity.java
@@ -0,0 +1,23 @@
+package com.example.smartlocking;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class LockScreenActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.test_display);
+		
+		//getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+		
+		Toast.makeText(LockScreenActivity.this, "ºñ¹øÆ²·È½º", Toast.LENGTH_SHORT).show();
+		
+		finish();
+	}
+}
