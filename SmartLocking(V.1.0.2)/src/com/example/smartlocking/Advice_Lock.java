db3daa6e11ba21a88de43ddae1299db040abc26e
TaeGu <mykimtaegu@naver.com>
2015-12-18 19:57:49 +0900

    V.1.0.2

---
+++ SmartLocking(V.1.0.2)/src/com/example/smartlocking/Advice_Lock.java
@@ -0,0 +1,15 @@
+package com.example.smartlocking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Advice_Lock extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.advice_lock);
+		
+		setTitle("SmartLocking");
+	}
+}
---
+++ SmartLocking(V.1.0.2)/src/com/example/smartlocking/Advice_USIM.java
@@ -0,0 +1,15 @@
+package com.example.smartlocking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Advice_USIM extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.advice_usim);
+		
+		setTitle("SmartLocking");
+	}
+}
